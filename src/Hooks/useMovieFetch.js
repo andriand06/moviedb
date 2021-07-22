da6c806878b34add32eefc696733a35f4258c8e7
andriand06 <64359593+andriand06@users.noreply.github.com>
2021-07-22 23:00:06 +0700

    0

--- src/Hooks/useMovieFetch.js
+++ src/Hooks/useMovieFetch.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import API from '../API'
 import { isPersistedState } from '../helpers'
  export const useMovieFetch = movieId => {
